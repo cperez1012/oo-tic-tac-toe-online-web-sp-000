d263a8833694e12fb62d78d93bcaac6d2499ef41
Christian Perez <perezchristian1012@gmail.com>
2019-08-13 18:02:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,16 +47,18 @@ class TicTacToe
   end
 
   def turn
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(index)
-    move(index, current_player)
-  else
-    turn
-  end
-    display_board
-end
+     puts "Please enter 1-9:"
+     input = gets.strip
+     index = input_to_index(input)
+     
+       if valid_move?(index)
+         move(index, current_player)
+        else
+         turn
+       end
+       display_board
+   end
+   
 
   def won?
     WIN_COMBINATIONS.detect do |win_combo|
